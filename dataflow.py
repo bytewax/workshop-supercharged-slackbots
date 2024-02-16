99476befff11397acf6a9d1307e31c1d0971fe68
Henrik Nyman <h@nyymanni.com>
2024-02-16 18:00:07 +0200

    Update dataflow.py template to have basics already in

--- dataflow.py
+++ dataflow.py
@@ -1,1 +1,26 @@
-# dataflow code
+"""A Bytewax stream for processing Slack messages."""
+
+import logging
+
+from bytewax.dataflow import Dataflow
+
+import dotenv
+
+
+def _build_dataflow() -> Dataflow:
+    flow = Dataflow("supercharged-slackbot")
+
+    return flow
+
+
+# Load environment variables from .env
+dotenv.load_dotenv()
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)-7s %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+# Dataflow needs to be assigned to a global variable called "flow"
+flow = _build_dataflow()
